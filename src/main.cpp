06b1144a0155dfa8b42b1932811a741603a954f8
victor-souza1997 <victor-souza1997@hotmail.com>
2022-03-20 22:10:44 -0300

    all functions with mqtt removed

--- src/main.cpp
+++ src/main.cpp
@@ -32,49 +32,38 @@
 #endif
 #include <ESPAsyncWebServer.h>
 
-
 // PainlessMesh credentials ( name, password and port ): You should change these
-#define   MESH_PREFIX     "whateverYouLike"
-#define   MESH_PASSWORD   "somethingSneaky"
-#define   MESH_PORT       5555
+#define MESH_PREFIX "whateverYouLike"
+#define MESH_PASSWORD "somethingSneaky"
+#define MESH_PORT 5555
 
 // WiFi credentials: should match your access point!
-#define   STATION_SSID     "AP_405"
-#define   STATION_PASSWORD "mercuryHg100"
+#define STATION_SSID "AP_405"
+#define STATION_PASSWORD "mercuryHg100"
 
-#define   HOSTNAME         "MQTT_Bridge"
-const char* serverName = "https://us-east-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/temperature_analysis-gjbaa/service/temperatura/incoming_webhook/api?secret=10minute" ;
+const char *serverName = "https://us-east-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/temperature_analysis-gjbaa/service/temperatura/incoming_webhook/api?secret=10minute";
 
-Scheduler userScheduler;   // to control your personal task
+Scheduler userScheduler; // to control your personal task
 
-painlessMesh  mesh;
+painlessMesh mesh;
 WiFiClient wifiClient;
 AsyncWebServer server(80);
 
 // Prototypes
-void receivedCallback( const uint32_t &from, const String &msg );
-void mqttCallback(char* topic, byte* payload, unsigned int length);
+void receivedCallback(const uint32_t &from, const String &msg);
 
 IPAddress getlocalIP();
-IPAddress myIP(0,0,0,0);
-IPAddress myAPIP(0,0,0,0);
-
-
-
-// hivemq pubblic broker address and port
-char mqttBroker[]  = "broker.hivemq.com";
-#define MQTTPORT 1883
+IPAddress myIP(0, 0, 0, 0);
+IPAddress myAPIP(0, 0, 0, 0);
 
 // topic's suffix: everyone can publish/subscribe to this public broker,
 // you have to change the following 2 defines
-#define PUBPLISHSUFFIX             "painlessMesh/from/"
-#define SUBSCRIBESUFFIX            "painlessMesh/to/"
+#define PUBPLISHSUFFIX "painlessMesh/from/"
+#define SUBSCRIBESUFFIX "painlessMesh/to/"
 
-#define PUBPLISHFROMGATEWAYSUFFIX  PUBPLISHSUFFIX "gateway"
+#define PUBPLISHFROMGATEWAYSUFFIX PUBPLISHSUFFIX "gateway"
 
-#define CHECKCONNDELTA 60     // check interval ( seconds ) for mqtt connection
-
-PubSubClient mqttClient;
+#define CHECKCONNDELTA 60 // check interval ( seconds ) for mqtt connection
 
 // TFT start
 #ifndef TFT_DISPOFF
@@ -82,20 +71,20 @@ PubSubClient mqttClient;
 #endif
 
 #ifndef TFT_SLPIN
-#define TFT_SLPIN   0x10
+#define TFT_SLPIN 0x10
 #endif
 
-#define TFT_MOSI        19
-#define TFT_SCLK        18
-#define TFT_CS          5
-#define TFT_DC          16
-#define TFT_RST         23
+#define TFT_MOSI 19
+#define TFT_SCLK 18
+#define TFT_CS 5
+#define TFT_DC 16
+#define TFT_RST 23
 
-#define TFT_BL          4   // Display backlight control pin
-#define ADC_EN          14  //ADC_EN is the ADC detection enable port
-#define ADC_PIN         34
-#define BUTTON_1        35
-#define BUTTON_2        0
+#define TFT_BL 4  // Display backlight control pin
+#define ADC_EN 14 // ADC_EN is the ADC detection enable port
+#define ADC_PIN 34
+#define BUTTON_1 35
+#define BUTTON_2 0
 #define TFT_BACKLIGHT_ON 0
 TFT_eSPI tft = TFT_eSPI(135, 240); // Invoke custom library
 // TFT end
@@ -104,205 +93,122 @@ Button2 btn2(BUTTON_2);
 
 bool calc_delay = false;
 SimpleList<uint32_t> nodes;
-uint32_t nsent=0;
+uint32_t nsent = 0;
 char buff[512];
-uint32_t nexttime=0;
-uint8_t  initialized=0;
+uint32_t nexttime = 0;
+uint8_t initialized = 0;
 
 StaticJsonDocument<500> doc; // <- a little more than 200 bytes in the stack
 
-//Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
-
+// Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
 
 void POSTData()
 {
-      
-      if(WiFi.status()== WL_CONNECTED){
-      HTTPClient http;
 
-      http.begin(serverName);
-      http.addHeader("Content-Type", "application/json");
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    HTTPClient http;
 
-      String json;
-      serializeJson(doc, json);
+    http.begin(serverName);
+    http.addHeader("Content-Type", "application/json");
 
-      Serial.println(json);
-      int httpResponseCode = http.POST(json);
-      Serial.print("resposta:");
-      Serial.println(httpResponseCode);
-      }
+    String json;
+    serializeJson(doc, json);
+
+    Serial.println(json);
+    int httpResponseCode = http.POST(json);
+    Serial.print("resposta:");
+    Serial.println(httpResponseCode);
+  }
 }
 
-
-// messages received from the mqtt broker
-void mqttCallback(char* topic, uint8_t* payload, unsigned int length) 
-  {
-  char* cleanPayload = (char*)malloc(length+1);
-  payload[length] = '\0';
-  memcpy(cleanPayload, payload, length+1);
-  String msg = String(cleanPayload);
-  free(cleanPayload);
-
-  Serial.printf("mc t:%s  p:%s\n", topic, payload);
-  
-  String targetStr = String(topic).substring(strlen(SUBSCRIBESUFFIX));
-  Serial.printf("targetStr:%s", targetStr);
-  if(targetStr == "gateway")
-  {
-    if(msg == "getNodes")
-      {
-      auto nodes = mesh.getNodeList(true);
-      String str;
-      for (auto &&id : nodes)
-        str += String(id) + String(" ");
-      mqttClient.publish( PUBPLISHFROMGATEWAYSUFFIX, str.c_str() );
-      }
-    if(msg == "getrt")
-      {
-      mqttClient.publish( PUBPLISHFROMGATEWAYSUFFIX, mesh.subConnectionJson(false).c_str() );
-      }
-    if(msg == "asnodetree")
-      {
-      //mqttClient.publish( PUBPLISHFROMGATEWAYSUFFIX, mesh.asNodeTree().c_str() );
-      }
-    }
-  else if(targetStr == "broadcast") 
-  {
-    mesh.sendBroadcast( msg );
-  }
-  else
-    {
-    uint32_t target = strtoul(targetStr.c_str(), NULL, 10);
-    if(mesh.isConnected(target))
-      {
-      mesh.sendSingle(target, msg);
-      }
-    else
-      {
-      mqttClient.publish(PUBPLISHFROMGATEWAYSUFFIX, "Client not connected!");
-      }
-    }
-  }
 // Needed for painless library
 
 // messages received from painless mesh network
-void receivedCallback( const uint32_t &from, const String &msg ) 
-  {
+void receivedCallback(const uint32_t &from, const String &msg)
+{
   Serial.printf("bridge: Received from %u msg=%s\n", from, msg.c_str());
   doc["sensor"]["temperature"] = msg.c_str();
-  doc["dispositivo"]["id"]=from;
+  doc["dispositivo"]["id"] = from;
 
   String topic = PUBPLISHSUFFIX + String(from);
   POSTData();
 
-  //Serial.println(topic);
-  //mqttClient.publish(topic.c_str(), msg.c_str());
-  }
-void newConnectionCallback(uint32_t nodeId) 
-  {
+  // Serial.println(topic);
+  // mqttClient.publish(topic.c_str(), msg.c_str());
+}
+void newConnectionCallback(uint32_t nodeId)
+{
   Serial.printf("--> Start: New Connection, nodeId = %u\n", nodeId);
   Serial.printf("--> Start: New Connection, %s\n", mesh.subConnectionJson(true).c_str());
-  }
-void changedConnectionCallback() 
-  {
+}
+void changedConnectionCallback()
+{
   Serial.printf("Changed connections\n");
 
   nodes = mesh.getNodeList();
   Serial.printf("Num nodes: %d\n", nodes.size());
   Serial.printf("Connection list:");
   SimpleList<uint32_t>::iterator node = nodes.begin();
-  while (node != nodes.end()) 
-    {
+  while (node != nodes.end())
+  {
     Serial.printf(" %u", *node);
     node++;
-    }
+  }
   Serial.println();
   calc_delay = true;
 
-  sprintf(buff,"Nodes:%d",nodes.size());
+  sprintf(buff, "Nodes:%d", nodes.size());
   tft.drawString(buff, 0, 32);
-  }
-void nodeTimeAdjustedCallback(int32_t offset) 
-  {
-  Serial.printf("Adjusted time %u Offset = %d\n", mesh.getNodeTime(),offset);
-  }
-void onNodeDelayReceived(uint32_t nodeId, int32_t delay)
-  {
-  Serial.printf("Delay from node:%u delay = %d\n", nodeId,delay);
-  }
-void reconnect()
-{
-  //byte mac[6];
-  char MAC[9];
-  int i;
-
-  // unique string
-  //WiFi.macAddress(mac);
-  //sprintf(MAC,"%02X",mac[2],mac[3],mac[4],mac[5]);
-  sprintf(MAC, "%08X",(uint32_t)ESP.getEfuseMac());  // generate unique addresss.
-  // Loop until we're reconnected
-  while (!mqttClient.connected()) 
-    {
-    Serial.println("Attempting MQTT connection...");    
-    // Attemp to connect
-    if (mqttClient.connect(/*MQTT_CLIENT_NAME*/MAC)) 
-      {
-      Serial.println("Connected");  
-      mqttClient.publish(PUBPLISHFROMGATEWAYSUFFIX,"Ready!");
-      mqttClient.subscribe(SUBSCRIBESUFFIX "#");
-      tft.drawString("Mqtt connected", 0, 96);
-      } 
-    else
-      {
-      Serial.print("Failed, rc=");
-      Serial.print(mqttClient.state());
-      Serial.println(" try again in 2 seconds");
-      // Wait 2 seconds before retrying
-      delay(2000);
-      mesh.update();
-      mqttClient.loop();
-      }
-    }
 }
-IPAddress getlocalIP() {return IPAddress(mesh.getStationIP());}
-String scanprocessor(const String& var)
+void nodeTimeAdjustedCallback(int32_t offset)
 {
-  if(var == "SCAN")
-    return mesh.subConnectionJson(false) ;
+  Serial.printf("Adjusted time %u Offset = %d\n", mesh.getNodeTime(), offset);
+}
+void onNodeDelayReceived(uint32_t nodeId, int32_t delay)
+{
+  Serial.printf("Delay from node:%u delay = %d\n", nodeId, delay);
+}
+
+IPAddress getlocalIP() { return IPAddress(mesh.getStationIP()); }
+String scanprocessor(const String &var)
+{
+  if (var == "SCAN")
+    return mesh.subConnectionJson(false);
   return String();
 }
-void setup() 
-  {
+void setup()
+{
   Serial.begin(115200);
 
   // TFT
   tft.init();
   tft.setRotation(1);
   tft.fillScreen(TFT_BLACK);
-  //tft.setTextSize(4);
-  tft.setTextColor(TFT_GREEN,TFT_BLACK);
+  // tft.setTextSize(4);
+  tft.setTextColor(TFT_GREEN, TFT_BLACK);
   tft.setCursor(0, 0);
   tft.setTextDatum(MC_DATUM);
   tft.setTextSize(2);
 
-  if (TFT_BL > 0) 
-    { // TFT_BL has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
-    pinMode(TFT_BL, OUTPUT); // Set backlight pin to output mode
+  if (TFT_BL > 0)
+  {                                         // TFT_BL has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
+    pinMode(TFT_BL, OUTPUT);                // Set backlight pin to output mode
     digitalWrite(TFT_BL, TFT_BACKLIGHT_ON); // Turn backlight on. TFT_BACKLIGHT_ON has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
-    }
+  }
   tft.fillScreen(TFT_BLACK);
   tft.setTextDatum(TL_DATUM);
   tft.drawString("MqttBridge 1.0.0", 0, 0);
-  //tft.setTextDatum(MC_DATUM);
-  //tft.drawString("LeftButton:", tft.width() / 2, tft.height() / 2 - 16);
+  // tft.setTextDatum(MC_DATUM);
+  // tft.drawString("LeftButton:", tft.width() / 2, tft.height() / 2 - 16);
 
-  //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | MSG_TYPES | REMOTE ); // all types on except GENERAL
-  //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
-  mesh.setDebugMsgTypes( ERROR | STARTUP | CONNECTION );  // set before init() so that you can see startup messages
+  // mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | MSG_TYPES | REMOTE ); // all types on except GENERAL
+  // mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+  mesh.setDebugMsgTypes(ERROR | STARTUP | CONNECTION); // set before init() so that you can see startup messages
 
   // Channel set to 1. Make sure to use the same channel for your mesh and for you other
   // network (STATION_SSID)
-  mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT, WIFI_AP_STA );
+  mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT, WIFI_AP_STA);
   mesh.onReceive(&receivedCallback);
   mesh.onNewConnection(&newConnectionCallback);
   mesh.onChangedConnections(&changedConnectionCallback);
@@ -315,25 +221,19 @@ void setup()
   mesh.setRoot(true);
   // This node and all other nodes should ideally know the mesh contains a root, so call this on all nodes
   mesh.setContainsRoot(true);
-
-  //Async webserver
+  // Async webserver
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-    {
+            {
     request->send(200, "text/html", "<form>Text to Broadcast<br><input type='text' name='BROADCAST'><br><br><input type='submit' value='Submit'></form>");
     if (request->hasArg("BROADCAST"))
       {
       String msg = request->arg("BROADCAST");
       mesh.sendBroadcast(msg);
-      }
-    });
+      } });
   server.on("/map", HTTP_GET, [](AsyncWebServerRequest *request)
-    {
-    request->send_P(200, "text/html", "<html><head><script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/vis/4.21.0/vis.js'></script><link href='https://cdnjs.cloudflare.com/ajax/libs/vis/4.21.0/vis-network.min.css' rel='stylesheet' type='text/css' /><style type='text/css'>#mynetwork {width: 1024px;height: 768px;border: 1px solid lightgray;}</style></head><body><h1>PainlessMesh Network Map</h1><div id='mynetwork'></div><a href=https://visjs.org>Made with vis.js<img src='http://www.davidefabbri.net/files/visjs_logo.png' width=40 height=40></a><script>var txt = '%SCAN%';</script><script type='text/javascript' src='http://www.davidefabbri.net/files/painlessmeshmap.js'></script></body></html>", scanprocessor );
-    });
+            { request->send_P(200, "text/html", "<html><head><script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/vis/4.21.0/vis.js'></script><link href='https://cdnjs.cloudflare.com/ajax/libs/vis/4.21.0/vis-network.min.css' rel='stylesheet' type='text/css' /><style type='text/css'>#mynetwork {width: 1024px;height: 768px;border: 1px solid lightgray;}</style></head><body><h1>PainlessMesh Network Map</h1><div id='mynetwork'></div><a href=https://visjs.org>Made with vis.js<img src='http://www.davidefabbri.net/files/visjs_logo.png' width=40 height=40></a><script>var txt = '%SCAN%';</script><script type='text/javascript' src='http://www.davidefabbri.net/files/painlessmeshmap.js'></script></body></html>", scanprocessor); });
   server.on("/scan", HTTP_GET, [](AsyncWebServerRequest *request)
-    {
-    request->send(200, "application/json", mesh.subConnectionJson(false) );
-    });
+            { request->send(200, "application/json", mesh.subConnectionJson(false)); });
   /*server.on("/asnodetree", HTTP_GET, [](AsyncWebServerRequest *request)
     {
     request->send(200, "text/html", mesh.asNodeTree().c_str() );
@@ -350,30 +250,19 @@ void setup()
   // THE ROLES ARE CORRECT
   mesh.initOTAReceive("bridge");
 
-  sprintf(buff,"Id:%d",mesh.getNodeId());
+  sprintf(buff, "Id:%d", mesh.getNodeId());
   tft.drawString(buff, 0, 16);
-  
-  mqttClient.setServer(mqttBroker, MQTTPORT);
-  mqttClient.setCallback(mqttCallback);  
-  mqttClient.setClient(wifiClient);
-  }
-void loop() 
-  {
+}
+void loop()
+{
   // it will run the user scheduler as well
   mesh.update();
-  mqttClient.loop();
 
-  if(myIP != getlocalIP())
-    {
+  if (myIP != getlocalIP())
+  {
     myIP = getlocalIP();
     Serial.println("My IP is " + myIP.toString());
     tft.drawString("Ip:" + myIP.toString(), 0, 80);
     initialized = 1;
-    }
-  if ( ( millis() >= nexttime ) && ( initialized ) )
-    {
-    nexttime=millis()+CHECKCONNDELTA*1000;
-    if (!mqttClient.connected()) 
-      {reconnect();}
-    }
   }
+}
