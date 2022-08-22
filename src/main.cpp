e4498082225a4a215780ee7a8dcc4e3e79a7a0c0
João Victor Lima de SOuza <victor-souza1997@hotmail.com>
2022-08-22 15:11:50 -0300

    fix: fixes problem with mesh connection
    Actually, not much was done. The real problem lied in the router. Because the channel was setted to auto, one couldn't connect to it.

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,10 @@
 // for more details
 // https://gitlab.com/painlessMesh/painlessMesh/wikis/bridge-between-mesh-and-another-network
 //************************************************************
+#include "IPAddress.h"
 #include "painlessMesh.h"
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 
 #define MESH_PREFIX "whateverYouLike"
 #define MESH_PASSWORD "somethingSneaky"
@@ -13,9 +16,72 @@
 #define STATION_SSID "AP_405"
 #define STATION_PASSWORD "mercuryHg100"
 #define STATION_PORT 5555
-uint8_t station_ip[4] = {192, 168, 1, 128}; // IP of the server
+#define CHANNEL 6
+// uint8_t station_ip[4] = {192, 168, 1, 128}; // IP of the server
 
+// mesh variavels
 painlessMesh mesh;
+AsyncWebServer server(80);
+IPAddress myIP(0, 0, 0, 0);
+IPAddress myAPIP(0, 0, 0, 0);
+
+// Function prototype
+IPAddress getlocalIP();
+void receivedCallback(uint32_t from, String &msg);
+void newConnectionCallback(uint32_t nodeId);
+void changedConnectionCallback();
+void nodeTimeAdjustedCallback(int32_t offset);
+
+void setup()
+{
+  Serial.begin(115200);
+  mesh.setDebugMsgTypes(ERROR | STARTUP | CONNECTION); // set before init() so that you can see startup messages
+
+  // Channel set to 6. Make sure to use the same channel for your mesh and for you other
+  // network (STATION_SSID)
+
+  mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT, WIFI_AP_STA);
+  // Setup over the air update support
+  // mesh.initOTA("bridge");
+
+  mesh.stationManual(STATION_SSID, STATION_PASSWORD);
+  //  Bridge node, should (in most cases) be a root node. See [the wiki](https://gitlab.com/painlessMesh/painlessMesh/wikis/Possible-challenges-in-mesh-formation) for some background
+  mesh.setRoot(true);
+  // This node and all other nodes should ideally know the mesh contains a root, so call this on all nodes
+  mesh.setContainsRoot(true);
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+
+  myAPIP = IPAddress(mesh.getAPIP());
+  Serial.println("My AP IP is " + myAPIP.toString());
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+    request->send(200, "text/html", "<form>Text to Broadcast<br><input type='text' name='BROADCAST'><br><br><input type='submit' value='Submit'></form>");
+    if (request->hasArg("BROADCAST")){
+      String msg = request->arg("BROADCAST");
+      mesh.sendBroadcast(msg);
+    } });
+  server.begin();
+}
+
+void loop()
+{
+  mesh.update();
+  // Serial.println(mesh.getStationIP());
+  if (myIP != getlocalIP())
+  {
+    myIP = getlocalIP();
+    Serial.println("My IP is " + myIP.toString());
+  }
+}
+
+IPAddress getlocalIP()
+{
+  return IPAddress(mesh.getStationIP());
+}
 
 // Needed for painless library
 void receivedCallback(uint32_t from, String &msg)
@@ -36,31 +102,4 @@ void changedConnectionCallback()
 void nodeTimeAdjustedCallback(int32_t offset)
 {
   Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(), offset);
-}
-
-void setup()
-{
-  Serial.begin(115200);
-  mesh.setDebugMsgTypes(ERROR | STARTUP | CONNECTION); // set before init() so that you can see startup messages
-
-  // Channel set to 6. Make sure to use the same channel for your mesh and for you other
-  // network (STATION_SSID)
-  mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT, WIFI_AP_STA);
-  // Setup over the air update support
-  // mesh.initOTA("bridge");
-
-  mesh.stationManual(STATION_SSID, STATION_PASSWORD, STATION_PORT, station_ip);
-  //  Bridge node, should (in most cases) be a root node. See [the wiki](https://gitlab.com/painlessMesh/painlessMesh/wikis/Possible-challenges-in-mesh-formation) for some background
-  mesh.setRoot(true);
-  // This node and all other nodes should ideally know the mesh contains a root, so call this on all nodes
-  mesh.setContainsRoot(true);
-  mesh.onReceive(&receivedCallback);
-  mesh.onNewConnection(&newConnectionCallback);
-  mesh.onChangedConnections(&changedConnectionCallback);
-  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
-}
-
-void loop()
-{
-  mesh.update();
 }
